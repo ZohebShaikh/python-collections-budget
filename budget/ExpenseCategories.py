be5a1e1772fe7b91de79c3bff9e8d6a05f48fe9f
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2021-09-13 13:00:43 +0530

    module 1 completed

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 def main():
     expenses = Expense.Expenses()
-    test commit
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
 
